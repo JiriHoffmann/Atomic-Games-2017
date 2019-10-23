53fcb40ca689a1363563f09ee8201f178f2f0934
Jiri Hoffmann <hoffik36@gmail.com>
2019-10-23 15:41:52 -0400

    Pathfinding part 1

--- sdks/node/pathing.js
+++ sdks/node/pathing.js
@@ -1,7 +1,59 @@
-
-
+var PF = require("pathfinding");
 
 function findPath(from, to, walls) {
+  let offset = 5;
+  let distanceX = Math.abs(from.x - to.x) + 1 + offset * 2;
+  let distanceY = Math.abs(from.y - to.y) + 1 + offset * 2;
 
+  let startX = from.x;
+  if (to.x < startX) {
+    startX = to.x;
+  }
+
+  let startY = from.y;
+  if (to.y < startY) {
+    startY = to.y;
+  }
+
+  var grid = new PF.Grid(distanceX, distanceY);
+  for (var i = 0; i < walls.length; i++) {
+    try {
+    
+      grid.setWalkableAt(
+        Math.abs(startX - walls[i].x) + offset,
+        Math.abs(startY - walls[i].y) + offset,
+        false
+      );
+    } catch {}
+  }
+  var finder = new PF.AStarFinder();
+  var path = finder.findPath(
+    Math.abs(startX - from.x) + offset,
+    Math.abs(startY - from.y) + offset,
+    Math.abs(startX - to.x) + offset,
+    Math.abs(startY - to.y) + offset,
+
+    grid
+  );
+  path.forEach(route => {
+    route[0] = route[0] + startX - offset;
+    route[1] = route[1] + startY - offset;
+  });
+
+  return path;
 }
-export { findPath };
+
+let from = { x: 3, y: 2 };
+let to = { x: 1, y: 5 };
+let walls = [
+  { x: 2, y: 4 },
+  { x: 2, y: 5 },
+  { x: 2, y: 6 },
+  { x: 2, y: 7 },
+  { x: 1, y: 3 },
+  { x: 1, y: 4 },
+  { x: 0, y: 3 }
+];
+console.log(findPath(from, to, walls));
+
+// export default findPath;
--- sdks/node/run.js
+++ sdks/node/run.js
@@ -1,107 +1,110 @@
-const NodeClient = require('./node-client');
+const NodeClient = require("./node-client");
 
-
-let ip = process.argv.length > 2 ? process.argv[2] : '127.0.0.1';
-let port = process.argv.length > 3 ? process.argv[3] : '8081';
+let ip = process.argv.length > 2 ? process.argv[2] : "127.0.0.1";
+let port = process.argv.length > 3 ? process.argv[3] : "8081";
 
 let map;
-
-let mapUnits = [];
-
-let client = new NodeClient(
-	ip,
-	port,
-	(dataUpdates) => {
-		updateMap(dataUpdates, map);
-		mapUnits = updateUnits(dataUpdates, mapUnits);
-	},
-	() => {
-		let cmds = generateCommands(mapUnits, map);
-		// console.log(cmds);
-		return cmds;
-	}
-);
-
 let resources = [];
 let nearest_resource = {};
 let walls = [];
+let enemy_base = {};
+let myUnits = [];
+let allUnits = [];
 
+let client = new NodeClient(
+  ip,
+  port,
+  dataUpdates => {
+    updateMap(dataUpdates, map);
+	myUnits = updateUnits(dataUpdates, myUnits);
+
+	allUnits = dataUpdates.unit_updates;
+    console.log(allUnits);
+  },
+  () => {
+    let cmds = generateCommands(myUnits, map);
+    // console.log(cmds);
+    return cmds;
+  }
+);
 
 function updateMap(dataUpdates, map) {
-	try {
-		let tiles = dataUpdates.tile_updates;
-		if (tiles !== []) {
-			tiles.forEach((tile) => {
-				updateResources(tile);
-				updateWalls(tile);
-			});
-		}
-	} catch (err) {
-		console.log(err);
-	}
+  try {
+    let tiles = dataUpdates.tile_updates;
+    if (tiles !== []) {
+      tiles.forEach(tile => {
+        updateResources(tile);
+        updateWalls(tile);
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 function updateWalls(tile) {
-	if (tile.blocked === true) {
-		checkWalls(tile) === true ? null : walls.push({ x: tile.x, y: tile.y });
-		console.log('Walls:', walls);
-	}
+  if (tile.blocked === true) {
+    checkWalls(tile) === true ? null : walls.push({ x: tile.x, y: tile.y });
+    // console.log("Walls:", walls);
+  }
 }
 
 function updateResources(tile) {
-	if (tile.resources !== null && tile.resources !== undefined) {
-		checkResources(tile) === true ? null : resources.push({ x: tile.x, y: tile.y, total: tile.resources.total });
-		console.log('Resources:', resources);
-		updateNearestResource();
-	}
+  if (tile.resources !== null && tile.resources !== undefined) {
+    checkResources(tile) === true
+      ? null
+      : resources.push({ x: tile.x, y: tile.y, total: tile.resources.total });
+    // console.log("Resources:", resources);
+    updateNearestResource();
+  }
 }
 
 function updateNearestResource() {
-	let distance = 1000;
-	resources.forEach((resource) => {
-		let tempDistance = Math.abs(resource.x) + Math.abs(resource.y);
-		if (tempDistance < distance) {
-			distance = tempDistance;
-			nearest_resource = resource;
-		}
-	});
-	console.log("Nearest Resource:", nearest_resource);
+  let distance = 1000;
+  resources.forEach(resource => {
+    let tempDistance = Math.abs(resource.x - resource.y);
+    if (tempDistance < distance) {
+      distance = tempDistance;
+      nearest_resource = resource;
+    }
+  });
+  //   console.log("Nearest Resource:", nearest_resource);
 }
 
 function checkWalls(tile) {
-	let contains = false;
-	walls.forEach((wall) => {
-		if (wall.x === tile.x && wall.y === tile.y) {
-			contains = true;
-		}
-	});
-	return contains;
+  let contains = false;
+  walls.forEach(wall => {
+    if (wall.x === tile.x && wall.y === tile.y) {
+      contains = true;
+    }
+  });
+  return contains;
 }
 
 function checkResources(tile) {
-	let contains = false;
-	resources.forEach((resource) => {
-		if (resource.x === tile.x && resource.y === tile.y) {
-			contains = true;
-		}
-	});
-	return contains;
+  let contains = false;
+  resources.forEach(resource => {
+    if (resource.x === tile.x && resource.y === tile.y) {
+      contains = true;
+    }
+  });
+  return contains;
 }
 
 function updateUnits(dataUpdates, units) {
-	// Update your units based on data updates
-	// Currently this code just maintains an array of your unit's ids
-	let ids = units.concat(dataUpdates.unit_updates.map((u) => u.id));
-	return ids.filter((val, idx) => ids.indexOf(val) === idx);
+  // Update your units based on data updates
+  // Currently this code just maintains an array of your unit's ids
+  let ids = units.concat(dataUpdates.unit_updates.map(u => u.id));
+  return ids.filter((val, idx) => ids.indexOf(val) === idx);
 }
 
 function generateCommands(units, map) {
-	// Generate commands here, currently just choosing a random
-	// unit and a random direction
-	return [
-		{
-			command: 'MOVE',
-			dir: [ 'N', 'E', 'S', 'W' ][Math.floor(Math.random() * 4)],
-			unit: units[Math.floor(Math.random() * units.length)]
-		}
-	];
+  // Generate commands here, currently just choosing a random
+  // unit and a random direction
+  return [
+    {
+      command: "MOVE",
+      dir: ["N", "E", "S", "W"][Math.floor(Math.random() * 4)],
+      unit: units[Math.floor(Math.random() * units.length)]
+    },
+  ];
 }
